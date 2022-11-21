d79ead984594d5862b6d7a6d97f0b4ea95e25fb7
CoolPolyester <Mkasper045@gmail.com>
2022-11-21 13:19:58 -0800

    Rework networking to make it create new keys dynamically for each tag

--- network.py
+++ network.py
@@ -1,9 +1,19 @@
 from _pynetworktables import NetworkTables
 
-NetworkTables.initialize(server="localhost")
+# Will need to set with ip of roboRIO when on the robot
+NetworkTables.initialize()
 vision_table = NetworkTables.getDefault().getTable("Vision")
 
 
-def log_pos(x, z):
-    vision_table.getEntry("x_pos").setValue(float(x))
-    vision_table.getEntry("z_pos").setValue(float(z))
+def log_pos(tag_id, x, y, z):
+    vision_table.getEntry(str(tag_id) + "x").setValue(float(x))
+    vision_table.getEntry(str(tag_id) + "y").setValue(float(y))
+    vision_table.getEntry(str(tag_id) + "z").setValue(float(z))
+
+
+def log_looptime(time):
+    vision_table.getEntry("Vision_Looptime").setValue(time)
+
+
+def flush():
+    NetworkTables.getDefault().flush()

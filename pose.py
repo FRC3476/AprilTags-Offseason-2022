17d467d10fc7f35d5920e966e50cf2f26effa12c
CoolPolyester <Mkasper045@gmail.com>
2022-11-21 13:19:18 -0800

    Reset offset map and give it a default value

--- pose.py
+++ pose.py
@@ -2,16 +2,21 @@
 # Format is (X_OFFSET, X_NORMALIZATION, Y_OFFSET, Y_NORMALIZATION, Z_OFFSET, Z_NORMALIZATION)
 
 offset_map = {
-    0: (.08, .17, .18, .29, 0, .8),
-    1: (0, 0, 0, 0, 0, 0),
-    2: (0, 0, 0, 0, 0, 0),
-    3: (0, 0, 0, 0, 0, 0)
+    -1: (0, 1, 0, 1, 0, 1),
+    0: (0, 1, 0, 1, 0, 1),
+    1: (0, 1, 0, 1, 0, 1),
+    2: (0, 1, 0, 1, 0, 1),
+    3: (0, 1, 0, 1, 0, 1),
+    4: (0, 1, 0, 1, 0, 1)
 }
+
+offset_map.setdefault(-1)
 
 
 # Find coordinates of tag (normalized to 0-1) (Orgin is left, bottom, far)
 def normalize_pose(detection):
     offsets = offset_map[detection.tag_id]
+
     x_pos = (detection.pose_t[0] + offsets[0]) / offsets[1]
     y_pos = (detection.pose_t[1] + offsets[2]) / offsets[3]
     z_pos = (detection.pose_t[2] + offsets[4]) / offsets[5]
